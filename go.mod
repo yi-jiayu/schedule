ca1fa94aee3d326bbffe20f1d205530b2696b3aa
Jiayu Yi <yijiayu@gmail.com>
2020-12-30 09:25:31 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yi-jiayu/schedule
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func requestLogger(w http.ResponseWriter, r *http.Request) {
+	req, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(req))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	requestLogger(w, r)
+	switch r.Method {
+	case http.MethodGet:
+		handleVerify(w, r)
+	case http.MethodPost:
+		handleUpdate(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func handleUpdate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleVerify(w http.ResponseWriter, r *http.Request) {
+	challenge := r.URL.Query().Get("hub.challenge")
+	_, _ = w.Write([]byte(challenge))
+}
+
+func main() {
+	err := http.ListenAndServe(":8080", http.HandlerFunc(handleRequest))
+	if err != nil {
+		panic(err)
+	}
+}
